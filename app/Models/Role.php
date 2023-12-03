29185c827bf64ce0e16e0bfe6e46611e4fc1bce7
ErickOcto <128987328+ErickOcto@users.noreply.github.com>
2023-12-03 11:12:07 +0800

    update model

--- app/Models/Role.php
+++ app/Models/Role.php
@@ -4,8 +4,28 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Role extends Model
 {
-    use HasFactory;
+    use HasFactory, SoftDeletes;
+
+    protected $dates = [
+        'created_at',
+        'updated_at',
+        'deleted_at'
+    ];
+
+    //declare fillable
+    protected $fillable = [
+        'user_id',
+        'type_user_id',
+        'contact',
+        'address',
+        'gender',
+        'created_at',
+        'updated_at',
+        'deleted_at'
+    ];
+
 }
