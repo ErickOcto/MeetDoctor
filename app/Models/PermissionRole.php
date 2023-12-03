ef2360e314f832429a14ba523ae44541842d95cb
ErickOcto <128987328+ErickOcto@users.noreply.github.com>
2023-12-03 11:12:01 +0800

    update model

--- app/Models/PermissionRole.php
+++ app/Models/PermissionRole.php
@@ -4,8 +4,25 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class PermissionRole extends Model
 {
-    use HasFactory;
+    use HasFactory, SoftDeletes;
+
+    public $table = 'permission_role';
+
+    protected $dates = [
+        'created_at',
+        'updated_at',
+        'deleted_at',
+    ];
+
+    protected $fillable = [
+        'permission_id',
+        'role_id',
+        'created_at',
+        'updated_at',
+        'deleted_at',
+    ];
 }
