a8ef056aca699d3c6696a9b30f0ac7fc2365aa31
ErickOcto <128987328+ErickOcto@users.noreply.github.com>
2023-12-03 10:58:11 +0800

    update model

--- app/Models/Transaction.php
+++ app/Models/Transaction.php
@@ -8,4 +8,15 @@ use Illuminate\Database\Eloquent\Model;
 class Transaction extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'appointment_id',
+        'config_payment_id',
+        'fee_doctor',
+        'fee_specialist',
+        'fee_hospital',
+        'sub_total',
+        'total',
+        'vat'
+    ];
 }
